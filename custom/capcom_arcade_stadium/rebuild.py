e3f6a00bd1cf01ba7949c18a3a7efda2659f2fd6
shawngmc <shawngmc@gmail.com>
2022-07-29 10:17:04 -0400

    start de-merging

--- custom/capcom_arcade_stadium/rebuild.py
+++ custom/capcom_arcade_stadium/rebuild.py
@@ -1,3 +1,4 @@
+import hashlib
 import glob
 import json
 import zipfile
@@ -20,8 +21,75 @@ def get_id_from_name(filename):
 def find_files(in_path):
     return glob.glob(in_path +'/*.pak')
 
+def swap_odd_even_bytes(contents):
+    contents_array = bytearray(len(contents))
+    contents_array[0::2] = contents[1::2]
+    contents_array[1::2] = contents[0::2]
+    return contents_array
+
+
+def even_swap_split(contents, chunk_count):
+    chunks = []
+    chunk_size = len(contents) // chunk_count
+    start_offset = 0
+    for i in range(0, chunk_count):
+        chunk = contents[start_offset:start_offset+chunk_size]
+        chunk = swap_odd_even_bytes(chunk)
+        start_offset = start_offset + chunk_size
+        chunks.append(chunk)
+    return chunks
+
+
+def cybotsu_split_test(contents, override_meta):
+    # Load the zip file contents
+    orig_data = dict()
+    with zipfile.ZipFile(io.BytesIO(contents), "r") as old_archive:
+        zip_entries = list(old_archive.infolist())
+
+        def getType(zip_entry):
+            return zip_entry.filename.split('.')[1]
+
+        for file_entry in zip_entries:
+            # read in the entry - we need the body either way
+            with old_archive.open(file_entry) as file_read_obj:
+                file_data = file_read_obj.read()
+                orig_data[getType(file_entry)] = file_data
+
+    # Start with the maincpu
+    curr_parts = override_meta['parts']['maincpu']
+    maincpu_file_names = list(map(lambda chunk: chunk['name'], curr_parts))
+    maincpu_chunks = even_swap_split(orig_data['maincpu'], len(maincpu_file_names))
+
+    for i in range(0, len(maincpu_file_names)):
+        chunk = maincpu_chunks[i]
+        name = maincpu_file_names[i]
+        m = hashlib.sha1()
+        m.update(chunk)
+        print(f'{name}: Checksum OK: {m.hexdigest() == curr_parts[i]["sha1"]}')
+
+        
+	# gfx
+	# ROM_LOAD64_WORD( "cyb.13m",   0x0000000, 0x400000, CRC(f0dce192) SHA1(b743938dc8e772dc3f63ed88a4a54c34fffdba21) )
+	# ROM_LOAD64_WORD( "cyb.15m",   0x0000002, 0x400000, CRC(187aa39c) SHA1(80e3cf5c69f13343de667e1476bb716d45d3ff63) )
+	# ROM_LOAD64_WORD( "cyb.17m",   0x0000004, 0x400000, CRC(8a0e4b12) SHA1(40132f3cc79b0a74460ebd4e0d4ddbe240efc06f) )
+	# ROM_LOAD64_WORD( "cyb.19m",   0x0000006, 0x400000, CRC(34b62612) SHA1(154bbceb7d303a208abb1b2f3d507d5afacc71ed) )
+	# ROM_LOAD64_WORD( "cyb.14m",   0x1000000, 0x400000, CRC(c1537957) SHA1(bfb1cc6786277b94ce28bfd464e2bbb6f6d3486e) )
+	# ROM_LOAD64_WORD( "cyb.16m",   0x1000002, 0x400000, CRC(15349e86) SHA1(b0cde577d29a9f4e718b673c8645529ef0ababc9) )
+	# ROM_LOAD64_WORD( "cyb.18m",   0x1000004, 0x400000, CRC(d83e977d) SHA1(e03f4a120c95a2f476ffc8492bca85e0c5cea068) )
+	# ROM_LOAD64_WORD( "cyb.20m",   0x1000006, 0x400000, CRC(77cdad5c) SHA1(94d0cc5f05de4bc2d43977d91f887005dc10310c) )
+
+    # audio cpu
+	# ROM_REGION( QSOUND_SIZE, "audiocpu", 0 ) // 64k for the audio CPU (+banks)
+	# ROM_LOAD( "cyb.01",   0x00000, 0x08000, CRC(9c0fb079) SHA1(06d260875a76da08d56ea2b2ae277e8c2dbae6e3) )
+	# ROM_CONTINUE(         0x10000, 0x18000 )
+	# ROM_LOAD( "cyb.02",   0x28000, 0x20000, CRC(51cb0c4e) SHA1(c322957558d8d3e9dad090aebbe485978cbce8f5) )
+
+    # qsound
+	# ROM_REGION( 0x400000, "qsound", 0 ) // QSound samples
+	# ROM_LOAD16_WORD_SWAP( "cyb.11m",   0x000000, 0x200000, CRC(362ccab2) SHA1(28e537067d4846f22657ee37166d18b8f05f4da1) )
+	# ROM_LOAD16_WORD_SWAP( "cyb.12m",   0x200000, 0x200000, CRC(7066e9cc) SHA1(eb6a9d4998b3311344d73bae88d661d81609c492) )
+
 def parse_kpka_archive(bytes):
-    
     magic_string = bytes[0:4].decode("utf-8")
     if magic_string != "KPKA":
         raise Exception("Not a valid KPKA archive!")
@@ -201,6 +269,11 @@ def main():
                             elif override['strategy'] == "demerge":
                                 print(f'********* Override for file offset {offset} demerge NYI!')
                                 filename = f'BAD_{id}_{offset}_{override["mame_name"]}_demerge_nyi.zip'
+                                with open(os.path.join(out_path, filename), "wb") as out_file:
+                                    out_file.write(contents)
+                            elif override['strategy'] == "cybotsu_split_test":
+                                filename = f'cybotsu.zip'
+                                new_contents = cybotsu_split_test(contents, override)
                                 with open(os.path.join(out_path, filename), "wb") as out_file:
                                     out_file.write(contents)
                             else:
