ba37059295659ca2424c9d1d0ed5943d697761a0
shawngmc <shawngmc@gmail.com>
2022-07-29 02:21:31 -0400

    much more complete CAS rebuild

---
+++ custom/capcom_arcade_stadium/readme.md
@@ -0,0 +1,43 @@
+Pulling CAS1 via old Steam Depots:
+
+
+The notes I found were at https://retropie.org.uk/forum/topic/10918/where-to-legally-acquire-content-to-play-on-retropie/606?lang=en-US. 
+Essentially, while a new version was added to steam, the old version can be specifically downloaded.
+An easier tool to pull the old versions can be found here: https://github.com/SteamRE/DepotDownloader
+
+After that, my python script and rom list can rebuild many of the ROMs. A couple still need retested (I ignored them at the time as they were dupes in my colleciton.)
+
+
+| Game                                             | Steam DepotID | Steam ManifestID     | Can Extract       | MAME Version | Filename         |
+|--------------------------------------------------|---------------|----------------------|-------------------|--------------|------------------|
+| Powered Gear: Strategic Variant Armor Equipment  | 1556723       | 305464856399220085   | Y                 | Mame 2010    | pgear.zip        |
+| Warriors of Fate                                 | 1556720       | 4950348891858375781  | Y                 | Mame 2010    | wofu.zip         |
+| Senjo no Okamill                                 | 1556714       | 4064909307706647901  | Y                 | Mame 2010    | mercsj.zip       |
+| Captain Commando                                 | 1556718       | 2975445377289899248  | Y                 | Mame Current | captcomm.zip     |
+| Street Fighter II                                | 1556717       | 7823458246757484640  | Y                 | Mame Current | sf2um.zip        |
+| Varth Operation Thunderstorm                     | 1556719       | 2957041054036881699  | Y                 | Mame Current | varth.zip        |
+| 1942                                             | 1556702       | 7110820738848130277  | Y                 | Mame Current | 1942.zip         |
+| Bionic Commando                                  | 1556707       | 3238877912290772385  | Y                 | Mame Current | bionicc2.zip     |
+| Commando                                         | 1556703       | 5154503909431550517  | Y                 | Mame Current | commandou.zip    |
+| Dynasty Wars                                     | 1556711       | 5906401061810674850  | Y                 | Mame Current | dynwar.zip       |
+| Final Fight                                      | 1556712       | 1427558570204465271  | Y                 | Mame Current | ffightu.zip      |
+| Forgotton Worlds                                 | 1556708       | 2495421590891474725  | Y                 | Mame Current | forgottnuaa.zip  |
+| Ghouls n' Ghosts                                 | 1556709       | 959652479204672528   | Y                 | Mame Current | ghoulsu.zip      |
+| Ghouls n' Ghosts                                 | 1556690       | 5034024650887104340  | Y                 | Mame Current | gng.zip          |
+| Pirate Ship Higemaru                             | 1556701       | 3568513687229079528  | Y                 | Mame Current | higemaru.zip     |
+| Legendary Wings                                  | 1556706       | 8788446018740704096  | Y                 | Mame Current | lwings.zip       |
+| Mega Twins                                       | 1556715       | 7832027131156162352  | Y                 | Mame Current | mtwins.zip       |
+| Section Z                                        | 1556704       | 7517264779277019420  | Y                 | Mame Current | sectionza.zip    |
+| Strider                                          | 1556710       | 8512960795726908906  | Y                 | Mame Current | striderua.zip    |
+| Street Fighter II Hyper Fighting                 | 1556721       | 317521835646003561   | Y                 | Mame Current | sf2hfu.zip       |
+| Tatakai no Banka                                 | 1556705       | 7200344018849026230  | Y                 | Mame Current | trojanj.zip      |
+| Vulgus                                           | 1556700       | 9143855046040096923  | Y                 | Mame Current | vulgus.zip       |
+| US Navy: Carrier Air Wing                        | 1556716       | 187920547906050452   | Y                 | Mame Current | cawingu.zip      |
+| Cyberbots: Fullmetal Madness                     | 1556724       | 4293976514552436962  | N - Merged files? | N/A          | N/A              |
+| 19XX: The War Against Destiny                    | 1556725       | 4171961223274458606  | N - Merged files? | N/A          | N/A              |
+| Battle Circuit                                   | 1556726       | 5500917457208551111  | N - Merged files? | N/A          | N/A              |
+| Giga Wing                                        | 1556727       | 1585945610334979859  | N - Merged files? | N/A          | N/A              |
+| 1944: The Loop Master                            | 1556728       | 3525105251099559918  | N - Merged files? | N/A          | N/A              |
+| Progear                                          | 1556729       | 6184184127241976915  | N - Merged files? | N/A          | N/A              |
+| 1941: Counter Attack                             | 1556713       | 2745567461535328718  | Y                 | Mame Current | 1941.zip         |
+| Super Street Fighter II Turbo                    | 1556722       | 2744654918197522324  | N - Merged files? | ?            | ssf2xj?          |
--- custom/capcom_arcade_stadium/rebuild.py
+++ custom/capcom_arcade_stadium/rebuild.py
@@ -1,89 +1,235 @@
-#!/bin/python3
-
 import glob
 import json
 import zipfile
+import sys
+import zlib
+import base64
+import logging, sys
+import os
+import io
 
-
-mame_subfolder_ids = [1556690, 1556700, 1556701, 1556702, 1556703, 1556704, 1556705, 1556706, 1556707, 1556708, 1556709, 1556710, 1556711, 1556713, 1556717, 1556718, 1556719, 1556720, 1556721, 1556723]
-
-mame_merged_ids = [1556722, 1556724, 1556725, 1556726, 1556727, 1556728, 1556729]
-
-mame_basefolder_ids = [1556712, 1556714, 1556715, 1556716]
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 def read_romlist():
     with open('romlist.json') as f:
         return json.load(f)
 
-
 def get_id_from_name(filename):
-    return filename[16:-4]
+    return filename[-11:-4]
 
+def find_files(in_path):
+    return glob.glob(in_path +'/*.pak')
 
-def find_files():
-    return glob.glob('./in/*.pak')
+def parse_kpka_archive(bytes):
+    
+    magic_string = bytes[0:4].decode("utf-8")
+    if magic_string != "KPKA":
+        raise Exception("Not a valid KPKA archive!")
+    else:
+        files = {}
+        logging.debug("KPKA detected!")
+        logging.debug(f'Archive Size: {len(bytes)}')
+        version = int.from_bytes(bytes[4:8], sys.byteorder)
+        logging.debug(f'Version: {version}')
+        total_files = int.from_bytes(bytes[8:12], sys.byteorder)
+        logging.debug(f'Number of files: {total_files}')
+        dummy_val = int.from_bytes(bytes[12:16], sys.byteorder)
 
+        curr_pos = 16
+        for x in range(0, total_files):
+            logging.debug(f'  file: {x}')
+            name_crc_l = int.from_bytes(bytes[curr_pos:curr_pos+4], sys.byteorder)
+            logging.debug(f'    name_crc_l: {name_crc_l}')
+            name_crc_u = int.from_bytes(bytes[curr_pos+4:curr_pos+8], sys.byteorder)
+            logging.debug(f'    name_crc_u: {name_crc_u}')
+            offset = int.from_bytes(bytes[curr_pos+8:curr_pos+16], sys.byteorder)
+            logging.debug(f'    offset: {offset}')
+            zsize = int.from_bytes(bytes[curr_pos+16:curr_pos+24], sys.byteorder)
+            logging.debug(f'    zsize: {zsize}')
+            size = int.from_bytes(bytes[curr_pos+24:curr_pos+32], sys.byteorder)
+            logging.debug(f'    size: {size}')
+            flag = int.from_bytes(bytes[curr_pos+32:curr_pos+40], sys.byteorder)
+            logging.debug(f'    flag: {flag}')
+            dummy_2 = int.from_bytes(bytes[curr_pos+40:curr_pos+44], sys.byteorder)
+            logging.debug(f'    dummy_2: {dummy_2}')
+            dummy_3 = int.from_bytes(bytes[curr_pos+44:curr_pos+48], sys.byteorder)
+            logging.debug(f'    dummy_3: {dummy_3}')
 
-def handle_subfolder_rom(rompath, metadata):
-    # create new zipfile
-    with zipfile.ZipFile(f"./out/{metadata['mamename']}.zip", "x") as new_archive:
-        # open old zipfile
-        with zipfile.ZipFile(rompath, "r") as old_archive:
-            # for each file in zipfile
-            for file_entry in old_archive.infolist(): 
-                # read zip file entry
-                print(file_entry)
+            # Handle the flag
+            if flag == 0 or flag == 1024:
+                contents = bytes[offset:offset+size]
+            elif flag == 1:
+                contents = bytes[offset:offset+zsize]
+                contents = zlib.decompress(contents, wbits = -15)
+                logging.debug(f'    decompressed size: {len(contents)} bytes')
+            elif flag == 2:
+                logging.warning("zstd NYI!")
+                contents = bytes[offset:offset+zsize]
+            else:
+                logging.warning(f'Compression flag {flag} NYI!')
+                logging.warning(f'    zsize: {zsize}')
+                logging.warning(f'    size: {size}')
+                contents = bytes[offset:offset+zsize]
+
+            files[offset] = contents
+            curr_pos = curr_pos + 48
+
+        return files
+
+def split_1941(contents):
+    # open old zipfile
+    with zipfile.ZipFile(io.BytesIO(contents), "r") as old_archive:
+        zip_entries = list(old_archive.infolist())
+
+        def getName(zip_entry):
+            return zip_entry.filename.split('/')[1]
+
+        out_files = dict()
+        new_contents = io.BytesIO()
+        with zipfile.ZipFile(new_contents, "w") as new_archive:
+            for file_entry in zip_entries:
+                # read in the entry - we need the body either way
                 with old_archive.open(file_entry) as file_read_obj:
                     file_data = file_read_obj.read()
-                    
-                    # change the name
-                    old_name = file_entry.filename
-                    new_name = old_name[len(metadata['mamename'])+1:len(old_name)]
-                    print(new_name)
+                    try:
+                        # if there is a slash, it's a 1941j file
+                        index = zip_entries[0].filename.index('/')
+                        new_archive.writestr(getName(file_entry), file_data)
 
-                    # add to new archive
-                    new_archive.writestr(new_name, file_data)
+                    except:
+                        # It's the 1941.zip
+                        out_files['1941.zip'] = file_data
+ 
+        
+        out_files[f'1941j.zip'] = new_contents.getvalue()
+        return out_files
 
 
-def handle_basefolder_rom(rompath, metadata):
-    # create new zipfile
-    with zipfile.ZipFile(f"./out/{metadata['mamename']}.zip", "x") as new_archive:
-        # open old zipfile
-        with zipfile.ZipFile(rompath, "r") as old_archive:
-            # for each file in zipfile
-            for file_entry in old_archive.infolist(): 
-                # read zip file entry
-                print(file_entry)
+def rebuild_mame_subfolder_zip(contents):
+    # open old zipfile
+    with zipfile.ZipFile(io.BytesIO(contents), "r") as old_archive:
+        zip_entries = list(old_archive.infolist())
+        
+        def getPrefix(zip_entry):
+            return zip_entry.filename.split('/')[0]
+
+        def getName(zip_entry):
+            return zip_entry.filename.split('/')[1]
+
+        # first, check the zip entries
+        try:
+            index = zip_entries[0].filename.index('/')
+        except Exception as e:
+            print(e)
+            print(zip_entries[0])
+            raise Exception(f'not a mame subfolder zip - no slash in first zip entry')
+
+        prefix = getPrefix(zip_entries[0])
+        for file_entry in zip_entries:
+            if getPrefix(file_entry) != prefix:
+                raise Exception(f'not a mame subfolder zip - {getPrefix(file_entry)} != {prefix}')
+
+        new_contents = io.BytesIO()
+        with zipfile.ZipFile(new_contents, "w") as new_archive:
+            for file_entry in zip_entries:
                 with old_archive.open(file_entry) as file_read_obj:
                     file_data = file_read_obj.read()
-
+ 
                     # add to new archive
-                    new_archive.writestr(file_entry.filename, file_data)
-
-
-def handle_merged_rom(rompath, metadata):
-    print("NYI")
-
+                    new_archive.writestr(getName(file_entry), file_data)
+        
+        ret_obj = dict()
+        ret_obj['filename'] = f'{prefix}.zip'
+        ret_obj['contents'] = new_contents.getvalue()
+        return ret_obj
 
 def main():
+    args = sys.argv[1:]
+    in_path = args[0]
+    out_path = args[1]
     romlist = read_romlist()["roms"]
-    files = find_files()
+    files = find_files(in_path)
     for file in files:
         id = get_id_from_name(file)
-        print(f"{file}: {id}")
-        if id in romlist:
-            rom_metadata = romlist[id]
+        logging.debug(f"{file}: {id}")
+        if id in romlist: 
+            rom_metadata = romlist[id]  
             print(rom_metadata)
-            if rom_metadata["method"] == "subfolder":
-                handle_subfolder_rom(file, rom_metadata)
-            elif rom_metadata["method"] == "basefolder":
-                handle_basefolder_rom(file, rom_metadata)
-            elif rom_metadata["method"] == "merged":
-                handle_merged_rom(file, rom_metadata)
-            else:
-                print("Unknown handling; skipping...")
+            try:
+                with open(file, "rb") as curr_file:
+                    file_content = bytearray(curr_file.read())
+                    kpka_contents = parse_kpka_archive(file_content)
+
+                    # Remove non-zip files
+                    kpka_zips = dict()
+                    for offset, contents in kpka_contents.items():
+                        if (contents[0:2].decode("utf-8") == "PK"):
+                            kpka_zips[offset] = contents
+
+                    def find_override(rom_metadata, offset):
+                        if not "overrides" in rom_metadata:
+                            return None
+                        
+                        overrides = rom_metadata['overrides']
+                        matches = [item for item in overrides if item['kpka_offset'] == offset]
+                        if (len(matches) == 0):
+                            return None
+                        elif (len(matches) == 1):
+                            return matches[0]
+                        else:
+                            raise Exception("Too many override matches!")
+                        
+
+                    for offset, contents in kpka_zips.items():
+                        override = find_override(rom_metadata, offset)
+                        if override:
+                            if not 'strategy' in override:
+                                print(f'********* Override for file offset {offset} has no strategy!')
+                                filename = f'BAD_{id}_{offset}_nyi.zip'
+                                with open(os.path.join(out_path, filename), "wb") as out_file:
+                                    out_file.write(contents)
+                            elif override['strategy'] == "rename":
+                                filename = f'{override["mame_name"]}.zip'
+                                with open(os.path.join(out_path, filename), "wb") as out_file:
+                                    out_file.write(contents)
+                            elif override['strategy'] == "split_1941":
+                                # This is a weird one - 1941.zip is in the zip, and 1941j/* is 1944j.zip
+                                new_files = split_1941(contents)
+                                for filename, contents in new_files.items():
+                                    with open(os.path.join(out_path, filename), "wb") as out_file:
+                                        out_file.write(contents)
+                            elif override['strategy'] == "demerge":
+                                print(f'********* Override for file offset {offset} demerge NYI!')
+                                filename = f'BAD_{id}_{offset}_{override["mame_name"]}_demerge_nyi.zip'
+                                with open(os.path.join(out_path, filename), "wb") as out_file:
+                                    out_file.write(contents)
+                            else:
+                                print(f'********* Override for file offset {offset} {override["strategy"]} NYI!')
+                                filename = f'BAD_{id}_{offset}_{override["strategy"]}_nyi.zip'
+                                with open(os.path.join(out_path, filename), "wb") as out_file:
+                                    out_file.write(contents)
+                        else:
+                            try:
+                                rebuilt = rebuild_mame_subfolder_zip(contents)
+                                with open(os.path.join(out_path, rebuilt['filename']), "wb") as out_file:
+                                    out_file.write(rebuilt['contents'])
+                            except Exception as e:
+                                print(e)
+            except Exception as e:
+                print(e)
+                print('Error While Opening the file!') 
+            # # if rom_metadata["method"] == "subfolder":
+            # #     handle_subfolder_rom(file, rom_metadata)
+            # # elif rom_metadata["method"] == "basefolder":
+            # #     handle_basefolder_rom(file, rom_metadata)
+            # # elif rom_metadata["method"] == "merged":
+            # #     handle_merged_rom(file, rom_metadata)
+            # if rom_metadata["method"] == "subzip":
+            #     handle_subzip_rom(file, rom_metadata)
+            # else:
+            #     print("Unknown handling; skipping...")
         else:
-            print("Not in romlist; skipping...")
+            logging.info(f'{file} not in romlist; skipping...')
 
 
 main()
